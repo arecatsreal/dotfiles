3db911b4b7fdf0c73d95ad962f7ab82302a46203
mip <n/a>
2020-08-06 12:15:15 -0500

    add polybar

---
+++ polybar/.config/polybar/launch.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload default &
+  done
+else
+  polybar --reload default &
+fi
